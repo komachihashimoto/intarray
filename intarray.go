78a4180ca54edb61cbb0a5a3c3b6a8af589fa1e8
komachihashimoto <joujinameko@gmail.com>
2024-01-09 13:42:58 +0900

    配列内の値の変更

--- intarray.go
+++ intarray.go
@@ -8,4 +8,6 @@ func main() {
 	fmt.Printf("0番目の要素は%d\n",intvals[0])
 	fmt.Printf("4番目の要素は%d\n",intvals[4])
 	fmt.Printf("2番目と3番目の要素を足すと%d\n",intvals[2] + intvals[3])
+	intvals[1] -= 100
+	fmt.Printf("今や1番目の要素は%d\n",intvals[1])
 }
